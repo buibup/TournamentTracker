c74829c37fb8ebdfddfa26c681503ff33f5a01a7
buibup <b5209194@gmail.com>
2017-08-30 23:41:29 +0700

    Initial

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represents one team in the matchup.
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represent the score for this particular team.
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// Represent the matchup that this team came from as the winner.
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
